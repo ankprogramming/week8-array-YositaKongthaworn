7875d65a189da25cf46a3fb7495a6ca88210f43a
Unknown <sodomyoindy@gmail.com>
2018-01-23 12:28:56 +0700

    เสร้จแล้ว

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -3,15 +3,36 @@
 using namespace std;
 
 int main () {
+//        char city[] ={'A','B','C','\0'};
+ //       char city2[] = {"ABC,DEF"}"
 
-        char name[] = {"Anukul"};
+        //การสร้างค่าเก็บตัวแปล โดยกำหนดชื่อ ขนาด จำนวนขนาด
+        char name[] = {"Yosita"};
         int nameLength = 6;
-        cout << name;
+        cout << name <<endl;
 
         // TODO: เขียนโปรแกรมเพื่อสลับชื่อ
         // hint : สร้าง array มารับค่าตัวอักษรแบบย้อนกลับ สามารถใช้ loop เข้ามาช่วยได้
 
+    char revNAME[nameLength+1];
 
+
+    for (int i = 0; i < nameLength; i++)
+    {
+
+        revNAME[i]= name[5-i];
+
+    }
+        revNAME[nameLength]= '\0';
+        cout<<revNAME<<endl ;
+
+
+
+        for (int i=0;i<nameLength;i++){
+            revNAME[i] =revNAME[i] +1;
+
+        }
+        cout<<revNAME;
 
         return 0;
 }
